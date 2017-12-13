dffbb40e3062624c7121b2f22ad97d2770c5d65d
amandakelly-compudat <amandakelly@compudatsystems.com>
2017-12-13 09:35:01 -0500

    12/12 changes

--- CTLab-Helpdesk/Controllers/HomeController.cs
+++ CTLab-Helpdesk/Controllers/HomeController.cs
@@ -9,14 +9,84 @@ namespace CTLabHelpdesk.Controllers
             return View();
         }
 
-        public IActionResult Tour()
+        public IActionResult MainScreen()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult MainScreen()
+        public IActionResult Administration()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult MemberMaint()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Annuity()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult ContractorMaint()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult ContractorRemit()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Reciprocity()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Pension()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult HealthWelfare()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Accounts()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult EoyProcessing()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Tour()
         {
             ViewData["Message"] = "Your application description page.";
 
